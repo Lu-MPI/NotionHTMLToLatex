68ce4e630093066b0af02f0a3a99c32ba05798dd
Lu-MPI <>
2022-01-17 14:31:44 +0800

    main.py parse arg and core func

---
+++ no2ml/main.py
@@ -0,0 +1,43 @@
+""" main file for package """
+import os
+import argparse
+from pathlib import Path
+
+
+def main():
+    """ main func for cmd """
+    print("no2ml: parse notion html to latex")
+
+    # argparse
+    parser = argparse.ArgumentParser(description='Test.')
+    parser.add_argument('source', type=str, help='The Source File')
+    parser.add_argument('target', type=str, help='The Target File')
+    args = parser.parse_args()
+
+    # io file
+    work_dir = os.getcwd()
+    source_file = Path(work_dir, args.source)
+    target_file = Path(work_dir, args.target)
+
+    # core
+    html = read_text(source_file)
+
+    latex = html2latex(html)
+
+    write_text(target_file, latex)
+
+
+def html2latex(html_text):
+    """ TODO func html2latex """
+    return ""
+
+
+def read_text(file) -> str:
+    """ read html to text """
+    return Path(file).read_text("UTF8")
+
+
+def write_text(filepath, text) -> str:
+    """ write latex text to file """
+    # TODO func write_latex
+    pass
